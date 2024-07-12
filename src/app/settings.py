17d8d6d59260f8ffc79cd982491e7c6d50c02662
laaraujo <lautaroarielaraujo@gmail.com>
2024-07-12 19:48:29 -0300

    make SECRET_KEY a required env var

--- src/app/settings.py
+++ src/app/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-33qdw)%@ow@650p&i9chxw56mnsvg=sp1a46qxvwtona7ry6)+"
+SECRET_KEY = os.environ.get["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
