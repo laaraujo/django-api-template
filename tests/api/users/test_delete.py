ad24fb943739857db0a1af570c84558ff37f9cf8
laaraujo <lautaroarielaraujo@gmail.com>
2024-09-06 15:51:32 -0300

    add

--- tests/api/users/test_delete.py
+++ tests/api/users/test_delete.py
@@ -3,12 +3,28 @@
 # Not found - 404
 # Unauthorized - 404 (Not 403, to avoid exposing data)
 
-def test__with_auth_admin_user__deletes_user(client, user_factory):
+import pytest
+from users.models import User
+
+
+def test__with_auth_user__deletes_user(client, user_factory):
     pwd = 'test'
     user = user_factory(password=pwd)
     client.force_authenticate(user)
     r = client.delete(f"/users/{user.id}/", data={"current_password": pwd})
     assert r.status_code == 204
-    # data = r.json()
-    # assert User.object.get
-    # assert
+    with pytest.raises(User.DoesNotExist):
+        assert user.refresh_from_db()
+
+def test__with_diff_user__returns_403(client, user, user_factory):
+    pwd = 'test'
+    diff_user = user_factory(password=pwd)
+    client.force_authenticate(user)
+    r = client.delete(f"/users/{diff_user.id}/", data={"current_password": pwd})
+    assert r.status_code == 403
+
+def test__with_anonymous_user__returns_401(client, user_factory):
+    pwd = 'test'
+    user = user_factory(password=pwd)
+    r = client.delete(f"/users/{user.id}/", data={"current_password": pwd})
+    assert r.status_code == 401
