c3a54a33f3a993c50136dc45e00e867e3194a1ba
laaraujo <lautaroarielaraujo@gmail.com>
2024-09-09 02:03:46 -0300

    remove unused serializer

--- src/users/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from users.models import User
-
-
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("email", "name")
